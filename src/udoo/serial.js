aead0ab6186ef708cec5d1879c7c689e2f44905b
theDweeb <sfurgurs@uncc.edu>
2019-03-29 14:44:39 -0400

    test

--- src/udoo/serial.js
+++ src/udoo/serial.js
@@ -55,7 +55,7 @@ parser.on('data', data =>{
     console.log("invalid request, no house 0");
   }
 
-  
+
   // Query asset balances
   else if(source != 0 && value == 0 && destination == 0) {
     console.log("Query function (serial.js)")
@@ -76,7 +76,7 @@ parser.on('data', data =>{
 
 
   // Consume function
-  else if(source == destination) {
+  else if(source === destination) {
     console.log("Consume function (serial.js)")
     resObj = {
       "owner": "House" + source, // sets the owner of the asset to "House #". This is just for clarity and has no effect on network
