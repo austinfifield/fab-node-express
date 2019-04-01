65348a2c375e9af6f5f24e4adc9850a60c83989e
theDweeb <sfurgurs@uncc.edu>
2019-04-01 17:11:04 -0400

    test

--- routes/assets.js
+++ routes/assets.js
@@ -14,7 +14,7 @@ router.post("/", (req, res) => {
     fabService.query("admin", constants.getEnergy,[resident.iden])
     // Promise to return payload
     .then(payload => {
-        res.send(JSON.parse(payload.value))
+        res.send(payload.value)
     })
     .catch((err) => {
         res.send(err);
