dcaaf7ff720fe7eae98ca85164d1e00dd0178779
theDweeb <sfurgurs@uncc.edu>
2019-04-08 12:09:21 -0400

    test

--- routes/fabric.js
+++ routes/fabric.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let data = require('../src/udoo/socket_testing').getData;
+let data = require('../src/udoo/serial.js').getData;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
