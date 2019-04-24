af41caa4849eb788ba6bbf0a01a7fc02f4fc4a69
theDweeb <sfurgurs@uncc.edu>
2019-04-24 17:21:23 -0400

    testing

--- src/udoo/serial.js
+++ src/udoo/serial.js
@@ -5,6 +5,8 @@ const tynt = require('tynt')
 const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
 const emitter = new EventEmitter();
+const axios = require('axios');
+
 
 port.on("open", () => {
     console.log(tynt.Blue('Serial port /dev/ttyACM0 is open\n'));
