59666f35431f969db797ff1f6864a18d0e716a5d
duke_block_8 <afifield@uncc.edu>
2019-05-02 14:44:53 -0400

    blockchain restart

---
+++ load_users_assets.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+
+python3 createUser.py 1
+sleep 4
+python3 createUser.py 2
+sleep 3
+python3 createUser.py 3
+sleep 3
+python3 createUser.py 4
+sleep 3
+python3 createUser.py 5
+sleep 3
+python3 createUser.py 6
+sleep 3
+python3 createAssets.py 1 20000 20 20000
+sleep 3
+python3 createAssets.py 2 20000 20 20000
+sleep 3
+python3 createAssets.py 3 20000 20 20000
+sleep 3
+python3 createAssets.py 4 20000 20 20000
+sleep 3
+python3 createAssets.py 5 20000 20 20000
+sleep 3
+python3 createAssets.py 6 20000 20 20000
+sleep 3
